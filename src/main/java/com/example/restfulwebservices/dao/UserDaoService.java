bfdc1a87d2058ee5e678bbd60bca4e90f544c45e
serg10arg <cabrerasergio094@gmail.com>
2025-09-29 11:43:02 -0300

    feat(user-service): Implementa patron de diseño DAO (Objeto de Acceso a Datos)
    Proporciona una interfaz para las operaciones CRUD de los usuarios.
    Esto desacopla la logica de acceso a datos de los controladores y
    servicios.

---
+++ src/main/java/com/example/restfulwebservices/dao/UserDaoService.java
@@ -0,0 +1,27 @@
+package com.example.restfulwebservices.dao;
+
+import com.example.restfulwebservices.model.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    // JPA/Hibernate > Database <-- Esto sería la forma "real" con una base de datos
+    // UserDaoService > Static List <-- Esto es lo que estamos haciendo una simple lista estática
+
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+}
