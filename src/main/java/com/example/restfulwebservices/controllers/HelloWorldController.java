c04d278d5850acd66be573105a82036e4f8932b4
serg10arg <cabrerasergio094@gmail.com>
2025-10-03 23:04:24 -0300

    feat(i18n): implementar un endpoint internacionalizado
    Se crea el HelloWorldController para demostrar la funcionalidad de
    internacionalizacion (i18n).
    
    El nuevo endpoint GET/hello-world-internacionalized utiliza
    MessageSource y LocalContextHolder para devolver un mensaje traducido
    basado en el encabezado Accept-Language de la solicitd.

---
+++ src/main/java/com/example/restfulwebservices/controllers/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.example.restfulwebservices.controllers;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+    }
+}
