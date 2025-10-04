594b8c238331693639d7c577c3bc5d58a251d190
serg10arg <cabrerasergio094@gmail.com>
2025-10-04 10:58:05 -0300

    feat(versioning): implementar estrategias de versionado de api
    Se introduce un controlador para demostrar cuatro enfoques diferentes
    para el versionado de una API RESt, junto con los DTOs PersonV1 y
    PersonV2 para representar las distintas versiones del recurso.
    
    El controlador implementa los siguientes metodos:
    	-Uri Versioning: a traves de /v1/person y /v2/person.
    	-Request Parameter: usando el parametro version en la URL
    	-Custom Header: mediante la cabecera X-API-VERSION.
    	-Media Type/Context Negotiation: con un produces personalizado
    	en la cabecera Accept.

---
+++ src/main/java/com/example/restfulwebservices/versioning/Name.java
@@ -0,0 +1,20 @@
+package com.example.restfulwebservices.versioning;
+
+public class Name {
+
+    private String firstName;
+    private String lastName;
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/main/java/com/example/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,20 @@
+package com.example.restfulwebservices.versioning;
+
+public class PersonV1 {
+
+    private String name;
+
+    public PersonV1(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV1 [name=" + name + "]";
+    }
+}
