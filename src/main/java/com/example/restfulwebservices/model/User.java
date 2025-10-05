c4d5d5d20ff0a2c461220a52ed54e5a022b182ad
serg10arg <cabrerasergio094@gmail.com>
2025-10-05 08:58:47 -0300

    feat(persistence): integrar jpa y h2 para la persistencia de usuarios
    Se migra el almacenamiento de datos de una lista estatica a una base de
    datos H2 en memoria utilizando Spring Data JPA.
    
    La clase User se convierte en una entidad JPA(@Entity). Se configura el
    datasource en application.properties y se añade un script data.sql para
    poblar la base de datos con datos de prueba al arrancar la aplicacion.

--- src/main/java/com/example/restfulwebservices/model/User.java
+++ src/main/java/com/example/restfulwebservices/model/User.java
@@ -1,21 +1,31 @@
 package com.example.restfulwebservices.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
 
+    protected User(){
+
+    }
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message= "Name should have atleast 2 characters")
-    @JsonProperty("user_name")
+    //@JsonProperty("user_name")
     private String name;
 
     @Past(message= "Birth Date should be in the past")
-    @JsonProperty("birth_date")
+    //@JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
